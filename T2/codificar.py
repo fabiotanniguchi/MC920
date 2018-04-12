f2ec4e9219997a28812321e1eafc9cd87fb90bda
Fábio Tanniguchi <ftanniguchi@gmail.com>
2018-04-11 23:17:16 -0300

    Starting reading and saving images in T2 and other definitions

---
+++ T2/codificar.py
@@ -0,0 +1,27 @@
+#-----------------------------------------------------------------------------------------
+# MC920 - TRABALHO 2
+#
+# FABIO TAKAHASHI TANNIGUCHI - RA 145980
+#
+#-----------------------------------------------------------------------------------------
+
+import sys as s
+from scipy import misc
+
+# python codificar.py imagem_entrada.png texto_entrada.txt plano_bits imagem_saida.png
+# python decodificar.py imagem_saida.png plano_bits texto_saida.txt
+
+#-----------------------------------------------------------------------------------------
+
+# receiving the parameters
+file_path = s.argv[1]
+text_path = s.argv[2]
+bits_plan = s.argv[3]
+result_file_path = s.argv[4]
+
+print("Lendo arquivo", file_path, "...")
+
+# loading the PNG into a ndarray
+image = misc.imread(file_path, False, "RGB")
+
+misc.imsave(result_file_path, image)
---
+++ T2/decodificar.py
@@ -0,0 +1,26 @@
+#-----------------------------------------------------------------------------------------
+# MC920 - TRABALHO 2
+#
+# FABIO TAKAHASHI TANNIGUCHI - RA 145980
+#
+#-----------------------------------------------------------------------------------------
+
+import sys as s
+from scipy import misc
+
+# python codificar.py imagem_entrada.png texto_entrada.txt plano_bits imagem_saida.png
+# python decodificar.py imagem_saida.png plano_bits texto_saida.txt
+
+#-----------------------------------------------------------------------------------------
+
+# receiving the parameters
+file_path = s.argv[1]
+bits_plan = s.argv[2]
+result_file_path = s.argv[3]
+
+print("Lendo arquivo", file_path, "...")
+
+# loading the PNG into a ndarray
+image = misc.imread(file_path, False, "RGB")
+
+misc.imsave(result_file_path, image)
