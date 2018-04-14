6274e213ae571e43fada43c5d460caa7d63b4766
Fábio Tanniguchi <ftanniguchi@gmail.com>
2018-04-14 20:07:02 -0300

    Coded-image done; little improvements

--- T2/codificar.py
+++ T2/codificar.py
@@ -20,11 +20,12 @@ text_path = s.argv[2]
 bits_plan = s.argv[3]
 result_file_path = s.argv[4]
 
-print("Lendo arquivo", file_path, "...")
+print("Carregando imagem em", file_path, "...")
 
 # loading the PNG into a ndarray
 image = misc.imread(file_path, False, "RGB")
 
+print("Lendo arquivo-texto em", text_path, "...")
 textfile  = open(text_path, 'r')
 text = ""
 for line in textfile:
@@ -40,6 +41,7 @@ bits = sp.array([( n >> bit) & 1 for bit in range(len(bin_text) - 1, -1, -1)])
 
 # resizing the array of bits into a ndarray with same shape of the image
 # when the new shape is bigger than the last one scipy fills with zero
+# if the new shape is smaller than the last one scipy will ignore the exceeded data
 bits.resize(image.shape)
 
 # preparing the ndarray to be used in a bitwise-or operation
@@ -48,5 +50,8 @@ bits = bits << int(bits_plan)
 # finally inserting the message into the image using a bitwise operation
 image = image | bits
 
+print("Salvando imagem codificada em", result_file_path, "...")
 # saving the image with the message
 misc.imsave(result_file_path, image)
+
+print("Finalizado!")
