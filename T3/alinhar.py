d96ae013ba0647152fbdc6493781e5846cce8d26
Fábio Tanniguchi <ftanniguchi@gmail.com>
2018-05-06 22:14:55 -0300

    Adding Hough Method

--- T3/alinhar.py
+++ T3/alinhar.py
@@ -11,12 +11,16 @@ import numpy as np
 
 from scipy import misc
 
+from skimage import feature
 from skimage import filters
+from skimage import img_as_uint
+from skimage import io
 from skimage import segmentation
 from skimage import transform
 
-from matplotlib import cm
-from matplotlib import pyplot as plt
+import statistics
+
+import warnings
 
 #-----------------------------------------------------------------------------------------
 
@@ -25,7 +29,7 @@ def alignHProj(image, out_file_path):
     max_amplitude_angle = 0
 
     for x in range(-90, 90):
-        rotated = transform.rotate(image, x, resize=True)
+        rotated = transform.rotate(image, x, resize=True, preserve_range=True)
         
         sum_lines = np.sum(rotated, axis=1)
         amplitude = np.max(sum_lines)
@@ -34,16 +38,33 @@ def alignHProj(image, out_file_path):
             max_amplitude = amplitude
             max_amplitude_angle = x
 
-    final_image = transform.rotate(image, max_amplitude_angle, resize=True)
+    final_image = transform.rotate(image, max_amplitude_angle, resize=True, preserve_range=True)
     final_image_for_pyplot = abs(final_image - 1)
 
-    plt.imsave(out_file_path, final_image_for_pyplot, cmap=cm.gray, vmin=0, vmax=1)
+    io.imsave(out_file_path, img_as_uint(final_image_for_pyplot))
 
     return 0
 
 #-----------------------------------------------------------------------------------------
 
 def alignHoughTransf(image, out_file_path):
+
+    blur = filters.gaussian(image, 3)
+    edges = feature.canny(blur)
+    hough_lines = transform.probabilistic_hough_line(edges)
+
+    slopes = [(y2 - y1) / (x2 - x1) if (x2 - x1) else 0 for (x1, y1), (x2, y2) in hough_lines]
+
+    rad_angles = [np.arctan(x) for x in slopes]
+    deg_angles = [np.degrees(x) for x in rad_angles]
+
+    rotation_number = statistics.median(deg_angles)
+
+    final_image = transform.rotate(image, rotation_number, resize=True, preserve_range=True)
+    final_image_for_pyplot = abs(final_image - 1)
+
+    io.imsave(out_file_path, img_as_uint(final_image_for_pyplot))
+
     return 0
 
 
@@ -54,22 +75,22 @@ file_path = s.argv[1]
 align_mode = s.argv[2]
 out_file_path = s.argv[3]
 
-print("Lendo arquivo", file_path, "...")
+with warnings.catch_warnings():
+    warnings.simplefilter("ignore")
 
-image = misc.imread(file_path, True)
+    print("Lendo arquivo", file_path, "...")
 
-threshold = filters.threshold_yen(image)
-bin_image = image <= threshold
-bin_image = segmentation.clear_border(bin_image)
+    image = misc.imread(file_path, True)
 
-plt.title("Imagem binária")
-plt.imshow(bin_image, cmap="Greys",  interpolation="nearest")
-plt.show()
+    threshold = filters.threshold_yen(image)
 
-if(align_mode == "1"):
-    alignHProj(bin_image, out_file_path)
-else:
-    if(align_mode == "2"):
+    bin_image = np.zeros(image.shape, dtype="bool")
+    bin_image = image <= threshold
+    bin_image = segmentation.clear_border(bin_image)
+
+    if(align_mode == "1"):
+        alignHProj(bin_image, out_file_path)
+    elif(align_mode == "2"):
         alignHoughTransf(bin_image, out_file_path)
     else:
         print()
