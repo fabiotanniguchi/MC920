d18aa843eb0e35b94b3e0941f156755f81809b15
Fábio Tanniguchi <ftanniguchi@gmail.com>
2018-05-09 00:05:43 -0300

    Agora sim com resultados bons

--- T3/alinhar.py
+++ T3/alinhar.py
@@ -11,9 +11,6 @@ import sys as s
 
 import numpy as np
 
-from scipy import misc
-from scipy import ndimage
-
 from skimage import feature
 from skimage import filters
 from skimage import segmentation
@@ -29,22 +26,28 @@ def alignHProj(image, out_file_path):
     max_amplitude = 0
     max_amplitude_angle = 0
 
-    for x in range(-90, 90):
-        rotated = misc.imrotate(image, x)
-        
-        sum_lines = np.sum(rotated, axis=1)
-        amplitude = np.max(sum_lines)
+    with warnings.catch_warnings():
+        warnings.simplefilter("ignore")
 
-        if (amplitude > max_amplitude):
-            max_amplitude = amplitude
-            max_amplitude_angle = x
+        for x in range(-90, 90):
+            rotated = transform.rotate(image, x, resize=True, order=0, clip=False, preserve_range=True)
 
-    final_image = abs(image - 1)
-    final_image = misc.imrotate(final_image, max_amplitude_angle, interp="bicubic")
+            sum_lines = np.sum(rotated, axis=1)
+            amplitude = np.max(sum_lines)
 
-    print()
-    print("Salvando arquivo", out_file_path, "...")
-    io.imwrite(out_file_path, final_image, "PNG-PIL", compress_level=0, optimize=False)
+            if (amplitude > max_amplitude):
+                max_amplitude = amplitude
+                max_amplitude_angle = x
+
+        final_image = abs(image - 1)
+        final_image = transform.rotate(final_image, max_amplitude_angle, resize=True, order=5, clip=False, preserve_range=True)
+
+        #plt.imshow(final_image, cmap="gray", vmin=0, vmax=1)
+        #plt.show()
+
+        print()
+        print("Salvando arquivo", out_file_path, "...")
+        io.imwrite(out_file_path, final_image, "PNG-PIL", compress_level=0, optimize=False)
 
     return 0
 
@@ -52,23 +55,26 @@ def alignHProj(image, out_file_path):
 
 def alignHoughTransf(image, out_file_path):
 
-    blur = filters.gaussian(image, 3)
-    edges = feature.canny(blur)
-    hough_lines = transform.probabilistic_hough_line(edges)
+    with warnings.catch_warnings():
+        warnings.simplefilter("ignore")
 
-    slopes = [(y2 - y1) / (x2 - x1) if (x2 - x1) else 0 for (x1, y1), (x2, y2) in hough_lines]
+        blur = filters.gaussian(image, 3)
+        edges = feature.canny(blur)
+        hough_lines = transform.probabilistic_hough_line(edges)
 
-    rad_angles = [np.arctan(x) for x in slopes]
-    deg_angles = [np.degrees(x) for x in rad_angles]
+        slopes = [(y2 - y1) / (x2 - x1) if (x2 - x1) else 0 for (x1, y1), (x2, y2) in hough_lines]
 
-    rotation_number = statistics.median(deg_angles)
+        rad_angles = [np.arctan(x) for x in slopes]
+        deg_angles = [np.degrees(x) for x in rad_angles]
 
-    final_image = abs(image - 1)
-    final_image = misc.imrotate(final_image, rotation_number, interp="bicubic")
+        rotation_number = statistics.median(deg_angles)
 
-    print()
-    print("Salvando arquivo", out_file_path, "...")
-    io.imwrite(out_file_path, final_image, "PNG-PIL", compress_level=0, optimize=False)
+        final_image = abs(image - 1)
+        final_image = transform.rotate(final_image, rotation_number, resize=True, order=5, clip=False, preserve_range=True)
+
+        print()
+        print("Salvando arquivo", out_file_path, "...")
+        io.imwrite(out_file_path, final_image, "PNG-PIL", compress_level=0, optimize=False)
 
     return 0
 
@@ -79,29 +85,26 @@ file_path = s.argv[1]
 align_mode = s.argv[2]
 out_file_path = s.argv[3]
 
-with warnings.catch_warnings():
-    warnings.simplefilter("ignore")
+print("Lendo arquivo", file_path, "...")
 
-    print("Lendo arquivo", file_path, "...")
+image = io.imread(file_path, "PNG-PIL", as_gray=True)
 
-    image = io.imread(file_path, "PNG-PIL", as_gray=True)
+threshold = filters.threshold_local(image, 17, offset=10)
 
-    threshold = filters.threshold_yen(image)
+bin_image = np.zeros(image.shape, dtype="bool")
+bin_image = image <= threshold
+bin_image = segmentation.clear_border(bin_image)
 
-    bin_image = np.zeros(image.shape, dtype="bool")
-    bin_image = image <= threshold
-    bin_image = segmentation.clear_border(bin_image)
-
-    if(align_mode == "1"):
-        alignHProj(bin_image, out_file_path)
-    elif(align_mode == "2"):
-        alignHoughTransf(bin_image, out_file_path)
-    else:
-        print()
-        print("Escolha uma opção válida")
-        print()
-        print("Modo 1 para Alinhamento por Projeção Horizontal")
-        print("Modo 2 para Alinhamento por Transformada de Hough")
+if(align_mode == "1"):
+    alignHProj(bin_image, out_file_path)
+elif(align_mode == "2"):
+    alignHoughTransf(bin_image, out_file_path)
+else:
+    print()
+    print("Escolha uma opção válida")
+    print()
+    print("Modo 1 para Alinhamento por Projeção Horizontal")
+    print("Modo 2 para Alinhamento por Transformada de Hough")
 
 print()
 print("FIM")
